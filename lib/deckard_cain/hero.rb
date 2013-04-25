4924624452463a81245d32ebaf3a9385029e333e
David Mills <dave@1morepx.com>
2013-04-24 21:54:25 -0400

    added some missing fields
    with tests

--- lib/deckard_cain/hero.rb
+++ lib/deckard_cain/hero.rb
@@ -1,9 +1,20 @@
+require 'date'
+
 class Hero
 
-  attr_accessor :name, :id, :level, :gender, :hardcore, :last_updated, :dead
+  attr_accessor :name, :id, :level, :gender, :hardcore, :last_updated, :dead, :paragon_level, :class
 
   def gender= value
     @gender = value.zero? ? :male : :female
+  end
+
+  def last_updated= value
+    @last_updated = DateTime.strptime("#{value}", '%s')
+  end
+
+  def class= value
+    value = value.gsub(/-/, '_')
+    @class = value.to_sym
   end
 
 end
--- test/hero_test.rb
+++ test/hero_test.rb
@@ -3,14 +3,33 @@ require 'deckard_cain'
 require 'deckard_cain/hero'
 
 class HeroTest   < Test::Unit::TestCase
-  def test_hero_gender_set
+  def test_gender_mutator
     male = Hero.new
     male.gender = 0
 
     female = Hero.new
     female.gender = 1
 
-    assert_equal male.gender, :male
-    assert_equal female.gender, :female
+    assert_equal male.gender   , :male
+    assert_equal female.gender , :female
+  end
+
+  def test_last_updated_mutator
+    hero = Hero.new
+    hero.last_updated = 1365373956
+
+    assert_equal hero.last_updated.year   , 2013
+    assert_equal hero.last_updated.month  , 4
+    assert_equal hero.last_updated.day    , 7
+    assert_equal hero.last_updated.hour   , 22
+    assert_equal hero.last_updated.minute , 32
+    assert_equal hero.last_updated.second , 36
+  end
+
+  def test_class_converts_to_symbol
+    hero = Hero.new
+    hero.class = "demon-hunter"
+
+    assert_equal hero.class , :demon_hunter
   end
 end
